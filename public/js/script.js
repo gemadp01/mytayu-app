40a699dac487282b5155d0d3ac30e706314eabde
Gema Dodi Pranata <gemadp01@gmail.com>
2023-08-09 17:35:19 +0700

    Folder Public

---
+++ public/js/script.js
@@ -0,0 +1,21 @@
+// Navbar
+const navbar = document.querySelector('nav');
+// const navbarNav = document.querySelectorAll('nav .navbar-nav .nav-link');
+const defaultActive = document.querySelector('nav .navbar-nav').firstElementChild;
+defaultActive.classList.add('active');
+let removeActive = defaultActive;
+// console.log(removeActive);
+
+navbar.addEventListener('click', function (e) {
+  if (e.target.className == 'nav-link') {
+    // console.log(e);
+    // setTimeout(function () {
+    //   e.target.classList.remove('active');
+    // }, 500);
+    if (removeActive != null) {
+      removeActive.classList.remove('active');
+    }
+    e.target.classList.add('active');
+    removeActive = e.target;
+  }
+});
